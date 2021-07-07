b3b6b9abf4cf46b65256c3039f1abd75d48c3de7
mohamed tamer <51374446+mohamedtamer0@users.noreply.github.com>
2021-07-07 09:25:17 +0200

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,87 @@
+var myVar = 42
+let myLet = 52
+
+print(myVar)
+print(myLet)
+
+let myIntgt = 70 
+let myDouble = 70.0 
+let myDouble2 : Double = 70
+print(myIntgt)
+print(myDouble)
+print(myDouble2)
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+print(widthLabel)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+
+let quotation = """
+I said "I have \(apples) apples."
+And then I said "I have \(apples + oranges) pieces of fruit."
+"""
+print(quotation)
+////////////////////////////////\
+
+var shoppingList = ["apple", "orange","water"]
+shoppingList[1] = "bottle of water"
+print(shoppingList)
+
+var occupations = [
+    "Malcolm" : "Captain",
+    "Kaylee" : "Mechanic",
+]
+
+occupations["Jayne"] = "Public Relations"
+
+shoppingList.append("blue paint")
+print(shoppingList)
+
+
+let emptyArray: [String] = []
+let emptyDic: [String: Float] = [:]
+
+//////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
